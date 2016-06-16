8d92eefd96cd8ff4fe6ce74b583e6c90fd7183f0
drey_up <dreyup96@gmail.com>
2016-06-16 23:55:31 +0600

    fix index

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -1,10 +1,13 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'Добро Пожаловать!';
+$this->title = 'Главная';
 
-$user = \app\models\User::find(Yii::$app->user->id);
+$user = \app\models\User::findOne(Yii::$app->user->id);
+
+
 ?>
 
 <div class="site-index">
- <h1> Вы вошли как <?= Yii::$app->user->can('admin') ? 'администратор ' : 'пользователь ';?> </h1>
+ <h2>Добро пожаловать, <?php if ($user->name != '' && $user->lname != '') echo $user->name.' '.$user->lname; else echo $user->username;?>!</h2>
+ <h2> Вы вошли как <?= Yii::$app->user->can('admin') ? 'администратор' : 'пользователь';?>.</h2>
 </div>
